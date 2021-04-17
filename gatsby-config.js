e9afff5d8811715d524b57017ca79dd8a72ef8e0
Adrian Hartanto <adrianfile1@gmail.com>
2021-04-17 22:24:58 +0700

    Test Title Work

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,7 +27,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: '' // This path is relative to the root of the site.
+        icon: './src/images/myphoto.png' // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-gatsby-cloud`,
