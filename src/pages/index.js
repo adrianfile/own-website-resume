c2827b1bf812e60b0ac90ca04c28b7330d2adb8b
Adrian Hartanto <adrianfile1@gmail.com>
2021-04-17 19:43:13 +0700

    Skkkrrrtt Rilis

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,27 +14,29 @@ const IndexPage = () => (
         <p className="text-xl md:text-2xl md:text-center">I'm a developer, i'm a planner, and i'm in learning to be going Master JavaScript.</p>
         <br></br>
         <button className="ml-40 md:mx-auto font-bold flex text-center px-2 bg-white text-black rounded-full hover:bg-indigo-600 hover:text-white transition delay-300 "
-          onClick={()=> scrollTo('#awal')}>
+          onClick={()=> scrollTo('#about')}>
           HIRE ME
         </button>
       </div>
     </div>
-    <div className="pb-96 md:flex">
+    <div id="about">
+      <div className="pb-96 md:flex">
       <div className="xl:ml-44 hidden xl:block overflow-hidden rounded-full">
         <StaticImage 
           src="../images/myphoto.png"
-          width={1000}
+          width={2000}
         />
       </div>
       <div className="xl:items-center xl:mt-28">
-        <p className=" font-bold text-center md:text-left md:mx-64 md:text-xl">
+        <p className=" font-bold text-center md:text-left md:mx-10 2xl:mx-64 md:text-xl">
           LET ME INTRODUCE MY SELF!
         </p>
         <br></br>
-        <p className="text-justify px-5 xl:px-64">
+        <p className="text-justify px-5 md:px-10 2xl:px-64">
             Hi World, my full name is Adrian Hartanto. You can call me Adri, Ian, Anto ,and Dre. i've learn Web Deveploment almost 2 years and 1 year for Software Development. I don't have any work experience on this skill on every company surely will be the new chick. I'm exciting to spend all my time to dreaming and make real a new world changer. 
         </p>
       </div>
+    </div>
     </div>
     <div>
       <div className="text-center font-bold">
