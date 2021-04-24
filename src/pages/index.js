37a2ae3067a3960519041601e15213ff15d2450a
Adrian Hartanto <adrianfile1@gmail.com>
2021-04-24 23:23:27 +0700

    Added About

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,7 @@ const IndexPage = () => (
       <div className="h-32 w-64 md:w-96 mx-auto text-white">
         <p className="text-3xl md:text-5xl md:text-center zilla">Adrian Hartanto</p>
         <br></br>
-        <p className="text-xl md:text-2xl md:text-center">I'm a developer, i'm a planner, and i'm in learning to be going Master JavaScript.</p>
+        <p className="text-xl md:text-2xl md:text-center">I'm a developer, i'm a planner, and i'm in learning to be going JavaScript Master.</p>
         <br></br>
         <button className="ml-20 md:mx-auto font-bold flex text-center px-2 bg-white text-black rounded-full hover:bg-indigo-600 hover:text-white transition delay-300 "
           onClick={()=> scrollTo('#about')}>
@@ -24,24 +24,44 @@ const IndexPage = () => (
         </button>
       </div>
     </div>
-    <div id="about">
-      <div className="pb-96 md:flex">
-      <div className="xl:ml-44 hidden xl:block overflow-hidden rounded-full">
-        <StaticImage 
-          src="../images/myphoto.png"
-          width={2000}
-        />
+    <div id="about" className="pb-96">
+      <div className="md:flex">
+        <div className="xl:ml-44 hidden xl:block overflow-hidden rounded-full">
+          <StaticImage 
+            src="../images/myphoto.png"
+            width={2000}
+          />
+        </div>
+        <div className="xl:items-center xl:mt-28">
+          <p className=" font-bold text-center md:text-left md:mx-10 2xl:mx-64 md:text-xl">
+            LET ME INTRODUCE MY SELF!
+          </p>
+          <br></br>
+          <p className="text-justify px-5 md:px-10 2xl:px-64">
+              Hi World, my full name is Adrian Hartanto. You can call me Adri, Ian, Anto ,and Dre. i've learn Web Development almost 2 years and 1 year for Software Development. I don't have any work experience on this skill on every company surely will be the new chick. I'm exciting to spend all my time to dreaming and make real a new world changer. 
+          </p>
+        </div>
       </div>
-      <div className="xl:items-center xl:mt-28">
-        <p className=" font-bold text-center md:text-left md:mx-10 2xl:mx-64 md:text-xl">
-          LET ME INTRODUCE MY SELF!
-        </p>
-        <br></br>
-        <p className="text-justify px-5 md:px-10 2xl:px-64">
-            Hi World, my full name is Adrian Hartanto. You can call me Adri, Ian, Anto ,and Dre. i've learn Web Deveploment almost 2 years and 1 year for Software Development. I don't have any work experience on this skill on every company surely will be the new chick. I'm exciting to spend all my time to dreaming and make real a new world changer. 
-        </p>
+      <div className="md:flex">
+        <div className="xl:items-center xl:mt-28 pt-10 block">
+          <p className=" font-bold text-center md:mx-10 2xl:mx-64 md:text-xl">
+            MY MISSION
+          </p>
+          <br></br>
+          <p className="text-justify px-5 md:px-10 2xl:px-64">
+              Freedom of Education, Bootcamp, and Educate People become a <i>SMARTGUY</i>
+          </p>
+        </div>
+        <div className="xl:items-center xl:mt-28 pt-10 block">
+          <p className=" font-bold text-center md:mx-10 2xl:mx-64 md:text-xl">
+            LAST WORK (WORK EXPERIENCE BEFORE RESIGN)
+          </p>
+          <br></br>
+          <p className="text-justify px-5 md:px-10 2xl:px-64">
+              Freelancer Wordpress Admin, Trial Account Creator, and Leader of Scratch Idea.
+          </p>
+        </div>
       </div>
-    </div>
     </div>
     <div>
       <div className="text-center font-bold">
@@ -108,6 +128,9 @@ const IndexPage = () => (
             </a>
         </div>
       </div>
+    </div>
+    <div id="footer" className="pt-10 text-center">
+      <p>2021. Created by <a className="text-purple-300" href="https://www.facebook.com/mhankskuy">Adrian Hartanto</a> using <a className="text-purple-300" href="https://www.gatsbyjs.com/">GatsbyJS</a></p>
     </div>
   </section>
 )
