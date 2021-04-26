d24282441a4352aeb29b1a7e0929cd21db82070e
Adrian Hartanto <adrianfile1@gmail.com>
2021-04-27 03:28:19 +0700

    Change Bottom Right Button

--- src/components/scrollTop.js
+++ src/components/scrollTop.js
@@ -23,9 +23,9 @@ const ScrollTop = () => {
     
     return (
         <div className={buttonTop ? 'opacity-100 transition ease-in-out duration-300' : 'opacity-0 transition ease-in-out duration-300'}>
-            <a id="buttonTop" className="w-12 bg-white rounded-full h-12 fixed z-50 bottom-10 right-5 text-center " href="#top">
+            <a id="buttonTop" className="w-12 bg-white rounded-full h-12 fixed z-50 bottom-12 right-5 text-center " href="#top">
                         <p className="mt-2 antonio text-black font-thin text-4xl">^</p>
-                        <p className="antonio">Ke-atas</p>
+                        <p className="antonio animate">Ke-atas</p>
             </a>
         </div>
     )
